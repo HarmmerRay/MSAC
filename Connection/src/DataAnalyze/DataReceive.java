9111c306e9020db23e0ac256c337a3f87d1766d3
HarmmerRay <2624773733q@gmail.com>
2023-08-28 10:12:10 +0800

    day2

---
+++ Connection/src/DataAnalyze/DataReceive.java
@@ -0,0 +1,14 @@
+package DataAnalyze;
+
+/**
+ * @projectName: MSAC
+ * @package: DataAnalyze
+ * @className: DataRecieve
+ * @author: HarmmerRay
+ * @description: TODO
+ * @date: 8/28/2023 10:10 AM
+ * @version: 1.0
+ */
+
+public class DataReceive {
+}
---
+++ Server/src/dao/TestDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+/**
+ * @projectName: MSAC
+ * @package: dao
+ * @className: TestDao
+ * @author: HarmmerRay
+ * @description: TODO
+ * @date: 8/28/2023 10:11 AM
+ * @version: 1.0
+ */
+
+public class TestDao {
+}
