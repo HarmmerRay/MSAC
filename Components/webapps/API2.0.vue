7f044335114ae3199ddf0175d401ecf3136666e1
HarmmerRay <zhao131804@gmail.com>
2023-09-10 12:35:26 +0800

    day1

---
+++ Components/webapps/API2.0.vue
@@ -0,0 +1,25 @@
+
+<script>
+export default {
+  data(){
+    return{
+      count:0
+    }
+  },
+  methods:{
+    increment(){
+      this.count++
+    }
+  },
+  mounted(){
+    console.log('The initial count is ${this.count}. ')
+  }
+}
+</script>
+
+<template>
+  <button @click = "increment">选项式API:Count is:{{count}}</button>
+</template>
+<style scoped>
+
+</style>
