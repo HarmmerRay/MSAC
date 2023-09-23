8d9c381d13aa71baa1552853d10bc58213c51c39
HarmmerRay <zhao131804@gmail.com>
2023-09-23 13:38:51 +0800

    day1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 package main.java;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -50,6 +49,93 @@ public class Main {
             return res;
         }
         return res;
+    }
+    //2560. 打家劫舍 IV
+    public int minCapability(int[] nums, int k) {
+        int res=0;
+        //1.建立元素是下标的数组 用nums[元素值] 排序 小的在前
+        int[] arr=new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = i;
+        }
+
+        //2.循环k次，加上k个数,期间判断两元素是否相邻
+        return res;
+    }
+    //2591. 将钱分给最多的儿童
+    public int distMoney(int money, int children) {
+        if(money==1){
+            return 0;
+        }
+
+        return (money - children) / 7;
+    }
+    //1993. 树上的操作
+    class LockingTree {
+        private int[] parent;
+        private int[] lockNodeUser;
+        private List<Integer>[] children;
+
+        public LockingTree(int[] parent) {
+            int n = parent.length;
+            this.parent = parent;
+            this.lockNodeUser = new int[n];
+            Arrays.fill(this.lockNodeUser, -1);
+            this.children = new List[n];
+            for (int i = 0; i < n; i++) {
+                this.children[i] = new ArrayList<Integer>();
+            }
+            for (int i = 0; i < n; i++) {
+                int p = parent[i];
+                if (p != -1) {
+                    children[p].add(i);
+                }
+            }
+        }
+
+        public boolean lock(int num, int user) {
+            if (lockNodeUser[num] == -1) {
+                lockNodeUser[num] = user;
+                return true;
+            }
+            return false;
+        }
+
+        public boolean unlock(int num, int user) {
+            if (lockNodeUser[num] == user) {
+                lockNodeUser[num] = -1;
+                return true;
+            }
+            return false;
+        }
+
+        public boolean upgrade(int num, int user) {
+            boolean res = lockNodeUser[num] == -1 && !hasLockedAncestor(num) && checkAndUnlockDescendant(num);
+            if (res) {
+                lockNodeUser[num] = user;
+            }
+            return res;
+        }
+
+        private boolean hasLockedAncestor(int num) {
+            num = parent[num];
+            while (num != -1) {
+                if (lockNodeUser[num] != -1) {
+                    return true;
+                }
+                num = parent[num];
+            }
+            return false;
+        }
+
+        private boolean checkAndUnlockDescendant(int num) {
+            boolean res = lockNodeUser[num] != -1;
+            lockNodeUser[num] = -1;
+            for (int child : children[num]) {
+                res |= checkAndUnlockDescendant(child);
+            }
+            return res;
+        }
     }
     public static void main(String[] args) {
         waysToBuyPensPencils(20,10,5);
