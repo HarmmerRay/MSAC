4a635fae9ab735f89b6ef3e19c0434148bfa91e9
HarmmerRay <zhao131804@gmail.com>
2023-10-04 22:13:12 +0800

    day2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -151,9 +151,38 @@ public class Main {
             }
             return sell2;
         }
+    // 188. 买卖股票的最佳时机 IV
+    class Solution {
+        public int maxProfit(int k, int[] prices) {
+            if (prices.length == 0) {
+                return 0;
+            }
+
+            int n = prices.length;
+            k = Math.min(k, n / 2);
+            int[][] buy = new int[n][k + 1];
+            int[][] sell = new int[n][k + 1];
+
+            buy[0][0] = -prices[0];
+            sell[0][0] = 0;
+            for (int i = 1; i <= k; ++i) {
+                buy[0][i] = sell[0][i] = Integer.MIN_VALUE / 2;
+            }
+
+            for (int i = 1; i < n; ++i) {
+                buy[i][0] = Math.max(buy[i - 1][0], sell[i - 1][0] - prices[i]);
+                for (int j = 1; j <= k; ++j) {
+                    buy[i][j] = Math.max(buy[i - 1][j], sell[i - 1][j] - prices[i]);
+                    sell[i][j] = Math.max(sell[i - 1][j], buy[i - 1][j - 1] + prices[i]);
+                }
+            }
+
+            return Arrays.stream(sell[n - 1]).max().getAsInt();
+        }
+    }
 
     public static void main(String[] args) {
         waysToBuyPensPencils(20,10,5);
-        System.out.println("远远不够");
+        System.out.println("铁骨铮铮");
     }
 }
